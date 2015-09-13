7ff4a8c7df34f2b4ae3a74b9ee31fc28c2857248
Keji Ren <kren@apple.com>
2015-09-13 16:37:37 -0700

    Updated printArray util methods.

--- java/src/main/java/solutions/utils/Utils.java
+++ java/src/main/java/solutions/utils/Utils.java
@@ -5,6 +5,10 @@ import org.apache.commons.lang3.ArrayUtils;
 public class Utils {
 
     public static void printArray(Object[] arr) {
+        if (arr == null || arr.length == 0) {
+            System.out.print("[]");
+            return;
+        }
         System.out.print("[");
         for (int i = 0; i < arr.length - 1; ++i) {
             System.out.print(arr[i].toString() + ", ");
@@ -14,6 +18,36 @@ public class Utils {
 
     public static void printArray(int[] arr) {
         Integer[] _arr = ArrayUtils.toObject(arr);
+        printArray(_arr);
+    }
+
+    public static void printArray(char[] arr) {
+        Character[] _arr = ArrayUtils.toObject(arr);
+        printArray(_arr);
+    }
+
+    public static void printArray(boolean[] arr) {
+        Boolean[] _arr = ArrayUtils.toObject(arr);
+        printArray(_arr);
+    }
+
+    public static void printArray(double[] arr) {
+        Double[] _arr = ArrayUtils.toObject(arr);
+        printArray(_arr);
+    }
+
+    public static void printArray(float[] arr) {
+        Float[] _arr = ArrayUtils.toObject(arr);
+        printArray(_arr);
+    }
+
+    public static void printArray(long[] arr) {
+        Long[] _arr = ArrayUtils.toObject(arr);
+        printArray(_arr);
+    }
+
+    public static void printArray(short[] arr) {
+        Short[] _arr = ArrayUtils.toObject(arr);
         printArray(_arr);
     }
 
