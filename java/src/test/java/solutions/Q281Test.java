04cdaba10ebd2ef3c14c1dc39dc9e48ff7f3020b
Keji Ren <kren@apple.com>
2015-09-13 11:58:28 -0700

    Updated in Q281Test.

--- java/src/test/java/solutions/Q281Test.java
+++ java/src/test/java/solutions/Q281Test.java
@@ -14,9 +14,8 @@ public class Q281Test extends TestCase {
     public void testZigzagIterator() throws Exception {
         List<Integer> v1 = new ArrayList<Integer>(Arrays.asList(1, 2));
         List<Integer> v2 = new ArrayList<Integer>(Arrays.asList(3, 4, 5, 6));
-        Q281 solution = new Q281();
-        ZigzagIterator zigzagIterator = solution.new ZigzagIterator(v1, v2);
-        int[] s1 = new int[6];
+        ZigzagIterator zigzagIterator = new Q281().new ZigzagIterator(v1, v2);
+        int[] s1 = new int[v1.size() + v2.size()];
         int i = 0;
         while (zigzagIterator.hasNext()) {
             s1[i++] = zigzagIterator.next();
