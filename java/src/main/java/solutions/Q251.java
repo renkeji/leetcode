10cf67e7f37fb804f0a958ac2a64981e5db24309
Keji Ren <kren@apple.com>
2015-09-14 23:41:11 -0700

    Flatten 2D Vector

--- java/src/main/java/solutions/Q251.java
+++ java/src/main/java/solutions/Q251.java
@@ -1,46 +1,49 @@
 package solutions;
 
-import solutions.datastructures.Interval;
-
-import java.util.*;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 
 /**
- * Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.
+ * Implement an iterator to flatten a 2d vector.
+ *
  * For example,
- *      Given [[0, 30],[5, 10],[15, 20]],
- *      return 2.
+ *      Given 2d vector =
+ *      [
+ *          [1,2],
+ *          [3],
+ *          [4,5,6]
+ *      ]
+ *
+ * By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,2,3,4,5,6].
  */
 
 public class Q251 extends Solution {
 
-    public int minMeetingRooms(Interval[] intervals) {
-        if (intervals == null || intervals.length == 0) {
-            return 0;
+    public class Vector2D {
+
+        private Iterator<List<Integer>> outer;
+        private Iterator<Integer> inner;
+
+        public Vector2D(List<List<Integer>> vec2d) {
+            this.outer = (vec2d != null && !vec2d.isEmpty()) ? vec2d.iterator() : Collections.<List<Integer>>emptyIterator();
+            this.inner = outer.hasNext() ? outer.next().iterator() : Collections.<Integer>emptyIterator();
         }
-        Arrays.sort(intervals, new Comparator<Interval>() {
-            @Override
-            public int compare(Interval o1, Interval o2) {
-                return o1.start - o2.start;
-            }
-        });
-        Map<Integer, Integer> map = new TreeMap<>();
-        for (Interval i : intervals) {
-            if (!map.containsKey(i.start)) {
-                map.put(i.start, 0);
-            }
-            map.put(i.start, map.get(i.start) + 1);
-            if (!map.containsKey(i.end)) {
-                map.put(i.end, 0);
-            }
-            map.put(i.end, map.get(i.end) - 1);
+
+        public int next() {
+            return inner.next();
         }
-        int rooms = 0;
-        int max = 0;
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            rooms += entry.getValue();
-            max = Math.max(max, rooms);
+
+        public boolean hasNext() {
+            while (!inner.hasNext()) {
+                if (!outer.hasNext()) {
+                    return false;
+                }
+                inner = outer.next().iterator();
+            }
+            return true;
         }
-        return max;
+
     }
 
 }
--- java/src/test/java/solutions/Q251Test.java
+++ java/src/test/java/solutions/Q251Test.java
@@ -1,20 +1,50 @@
 package solutions;
 
 import junit.framework.TestCase;
-import solutions.datastructures.Interval;
+import solutions.Q251.Vector2D;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Q251Test extends TestCase {
 
     public void testMinMeetingRooms() throws Exception {
-        Q251 solution = new Q251();
+        List<List<Integer>> lists = new ArrayList<>();
+        lists.add(new ArrayList<>(Arrays.asList(1, 2)));
+        lists.add(new ArrayList<>(Arrays.asList(3)));
+        lists.add(new ArrayList<>(Arrays.asList(4, 5, 6)));
+        Vector2D vector2D = new Q251().new Vector2D(lists);
+        List<Integer> actual = new ArrayList<>(6);
+        while (vector2D.hasNext()) {
+            actual.add(vector2D.next());
+        }
+        List<Integer> expected = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        assertEquals(expected, actual);
 
-        Interval[] intervals = new Interval[]{new Interval(0, 30), new Interval(5, 10), new Interval(15, 20)};
-        int rooms = solution.minMeetingRooms(intervals);
-        assertEquals(2, rooms);
+        lists.clear();
+        lists.add(new ArrayList<>(Arrays.asList(1)));
+        lists.add(Collections.<Integer>emptyList());
+        vector2D = new Q251().new Vector2D(lists);
+        actual = new ArrayList<>(1);
+        while (vector2D.hasNext()) {
+            actual.add(vector2D.next());
+        }
+        expected = new ArrayList<>(Arrays.asList(1));
+        assertEquals(expected, actual);
 
-        intervals = new Interval[]{new Interval(1, 5), new Interval(8, 9), new Interval(8, 9)};
-        rooms = solution.minMeetingRooms(intervals);
-        assertEquals(2, rooms);
+        lists.clear();
+        lists.add(new ArrayList<>(Arrays.asList(1, 2)));
+        lists.add(Collections.<Integer>emptyList());
+        lists.add(new ArrayList<>(Arrays.asList(3, 4)));
+        vector2D = new Q251().new Vector2D(lists);
+        actual = new ArrayList<>(1);
+        while (vector2D.hasNext()) {
+            actual.add(vector2D.next());
+        }
+        expected = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        assertEquals(expected, actual);
     }
 
 }
