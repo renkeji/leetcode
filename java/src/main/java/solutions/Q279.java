8f49ead87b34f705b37c001692d6f6ad9c14d4d0
Keji Ren <kren@apple.com>
2015-09-19 14:58:03 -0700

    Perfect Squares

--- java/src/main/java/solutions/Q279.java
+++ java/src/main/java/solutions/Q279.java
@@ -4,6 +4,13 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+ *
+ * For example,
+ *  given n = 12, return 3 because 12 = 4 + 4 + 4; given n = 13, return 2 because 13 = 4 + 9.
+ */
+
 public class Q279 extends Solution {
 
     // Top-down HashMap Cache solution -- StackOverflowException
