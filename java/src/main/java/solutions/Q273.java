88c66222409da1481f955768d96886fa7c173cf1
Keji Ren <kren@apple.com>
2015-09-19 14:52:44 -0700

    Integer to English Words

--- java/src/main/java/solutions/Q273.java
+++ java/src/main/java/solutions/Q273.java
@@ -1,5 +1,14 @@
 package solutions;
 
+/**
+ * Convert a non-negative integer to its english words representation. Given input is guaranteed to be less than 231 - 1.
+ *
+ * For example,
+ *  123 -> "One Hundred Twenty Three"
+ *  12345 -> "Twelve Thousand Three Hundred Forty Five"
+ *  1234567 -> "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
+ */
+
 public class Q273 extends Solution {
 
     public String convertThree(int n) {
