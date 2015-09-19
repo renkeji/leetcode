d890f344ea59c8f7c565fa0f7b49dc7cf2f254ba
Keji Ren <kren@apple.com>
2015-09-19 14:58:48 -0700

    Wiggle Sort

--- java/src/main/java/solutions/Q280.java
+++ java/src/main/java/solutions/Q280.java
@@ -2,6 +2,13 @@ package solutions;
 
 import java.util.Arrays;
 
+/**
+ * Given an unsorted array nums, reorder it in-place such that nums[0] <= nums[1] >= nums[2] <= nums[3]....
+ *
+ * For example,
+ *  given nums = [3, 5, 2, 1, 6, 4], one possible answer is [1, 6, 2, 5, 3, 4].
+ */
+
 public class Q280 extends Solution {
 
     public void wiggleSort(int[] nums) {
