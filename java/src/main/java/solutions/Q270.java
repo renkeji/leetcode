869d904229a38d27db0bab4a066d1810d8e4bab8
Keji Ren <kren@apple.com>
2015-09-19 14:49:00 -0700

    Closest Binary Search Tree Value

--- java/src/main/java/solutions/Q270.java
+++ java/src/main/java/solutions/Q270.java
@@ -2,6 +2,14 @@ package solutions;
 
 import solutions.datastructures.TreeNode;
 
+/**
+ * Given a non-empty binary search tree and a target value, find the value in the BST that is closest to the target.
+ *
+ * Note:
+ *  Given target value is a floating point.
+ *  You are guaranteed to have only one unique value in the BST that is closest to the target.
+ */
+
 public class Q270 extends Solution {
 
     public int closestValue(TreeNode root, double target) {
