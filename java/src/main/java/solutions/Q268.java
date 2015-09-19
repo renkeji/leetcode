c232ac6c7b9c86238afcf90f670c9a64553326b4
Keji Ren <kren@apple.com>
2015-09-19 14:48:12 -0700

    Missing Number

--- java/src/main/java/solutions/Q268.java
+++ java/src/main/java/solutions/Q268.java
@@ -1,5 +1,15 @@
 package solutions;
 
+/**
+ * Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+ *
+ * For example,
+ *  Given nums = [0, 1, 3] return 2.
+ *
+ * Note:
+ *  Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
+ */
+
 public class Q268 extends Solution {
 
     // TODO: need to consider the overflow case
