20f30d39294fb54eeafdbd6689b58b7dfc5773f3
Keji Ren <kren@apple.com>
2015-09-19 17:39:00 -0700

    Factor Combinations

--- java/src/main/java/solutions/Q254.java
+++ java/src/main/java/solutions/Q254.java
@@ -11,37 +11,35 @@ import java.util.List;
  * Write a function that takes an integer n and return all possible combinations of its factors.
  *
  * Note:
- *      1. Each combination's factors must be sorted ascending, for example: The factors of 2 and 6 is [2, 6], not [6, 2].
- *      2. You may assume that n is always positive.
- *      3. Factors should be greater than 1 and less than n.
+ *  1. Each combination's factors must be sorted ascending, for example: The factors of 2 and 6 is [2, 6], not [6, 2].
+ *  2. You may assume that n is always positive.
+ *  3. Factors should be greater than 1 and less than n.
  *
  * Examples:
- *      input: 1
- *      output:
- *          []
+ *  input: 1
+ *  output: []
  *
- *      input: 37
- *      output:
- *          []
+ *  input: 37
+ *  output: []
  *
- *      input: 12
- *      output:
- *          [
- *              [2, 6],
- *              [2, 2, 3],
- *              [3, 4]
- *          ]
+ *  input: 12
+ *  output:
+ *      [
+ *          [2, 6],
+ *          [2, 2, 3],
+ *          [3, 4]
+ *      ]
  *
- *      input: 32
- *      output:
- *          [
- *              [2, 16],
- *              [2, 2, 8],
- *              [2, 2, 2, 4],
- *              [2, 2, 2, 2, 2],
- *              [2, 4, 4],
- *              [4, 8]
- *          ]
+ *  input: 32
+ *  output:
+ *      [
+ *          [2, 16],
+ *          [2, 2, 8],
+ *          [2, 2, 2, 4],
+ *          [2, 2, 2, 2, 2],
+ *          [2, 4, 4],
+ *          [4, 8]
+ *      ]
  */
 
 public class Q254 extends Solution {
