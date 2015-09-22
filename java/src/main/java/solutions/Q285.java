82849f87e63b82073b2f161b916b261a356f61b3
Keji Ren <kren@apple.com>
2015-09-22 12:06:45 -0700

    Inorder Successor in BST

---
+++ java/src/main/java/solutions/Q285.java
@@ -0,0 +1,30 @@
+package solutions;
+
+import solutions.datastructures.TreeNode;
+
+/**
+ * Given a binary search tree and a node in it, find the in-order successor of that node in the BST.
+ *
+ * Note: If the given node has no in-order successor in the tree, return null.
+ */
+
+public class Q285 extends Solution {
+
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        if (root == null || p == null) {
+            return null;
+        } else {
+            if (p.val < root.val) {
+                TreeNode left = inorderSuccessor(root.left, p);
+                if (left != null) {
+                    return left;
+                }
+            }
+            if (p.val < root.val) {
+                return root;
+            }
+            return inorderSuccessor(root.right, p);
+        }
+    }
+
+}
