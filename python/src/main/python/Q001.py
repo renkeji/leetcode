587d1a7d582bbf69b8af6dde422f6cb4c55deb8d
Keji Ren <kren@apple.com>
2015-10-15 00:48:25 -0700

    Two Sum

---
+++ python/src/main/python/Q001.py
@@ -0,0 +1,22 @@
+from src.main.python.Solution import Solution
+
+
+class Q001(Solution):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        numbers = [(n, i+1) for i, n in enumerate(nums)]
+        numbers.sort(key=lambda t: t[0])
+        i, j = 0, len(numbers) - 1
+        while i < j:
+            sum = numbers[i][0] + numbers[j][0]
+            if sum == target:
+                return [numbers[i][1], numbers[j][1]] if numbers[i][1] <= numbers[j][1] else [numbers[j][1], numbers[i][1]]
+            elif sum <target:
+                i += 1
+            else:
+                j -= 1
+        return []
