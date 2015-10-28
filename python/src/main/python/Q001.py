4655c335099aecd652e9b5d84f5d5050e6d87ecf
Keji Ren <kren@apple.com>
2015-10-27 21:21:11 -0700

    Two Sum

--- python/src/main/python/Q001.py
+++ python/src/main/python/Q001.py
@@ -25,7 +25,8 @@ class Q001(Solution):
         while i < j:
             sum = numbers[i][0] + numbers[j][0]
             if sum == target:
-                return [numbers[i][1], numbers[j][1]] if numbers[i][1] <= numbers[j][1] else [numbers[j][1], numbers[i][1]]
+                return [numbers[i][1], numbers[j][1]] if numbers[i][1] <= numbers[j][1] \
+                    else [numbers[j][1], numbers[i][1]]
             elif sum < target:
                 i += 1
             else:
