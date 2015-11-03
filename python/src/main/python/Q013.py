12a4d27038f4ef91da28a6424205920319c61fbb
Keji Ren <kren@apple.com>
2015-11-03 00:16:13 -0800

    Roman to Integer

---
+++ python/src/main/python/Q013.py
@@ -0,0 +1,23 @@
+from src.main.python.Solution import Solution
+
+# Given a roman numeral, convert it to an integer.
+#
+# Input is guaranteed to be within the range from 1 to 3999.
+
+
+class Q013(Solution):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        numerals = { "M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1 }
+        sum = 0
+        s = s[::-1]
+        last = None
+        for x in s:
+            if last and numerals[x] < last:
+                sum -= 2 * numerals[x]
+            sum += numerals[x]
+            last = numerals[x]
+        return sum
