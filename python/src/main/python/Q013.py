944c4ad3e3f455384df146c0b4b4b7a61da855bf
Keji Ren <kren@apple.com>
2015-11-03 00:17:36 -0800

    Roman to Integer

--- python/src/main/python/Q013.py
+++ python/src/main/python/Q013.py
@@ -11,13 +11,13 @@ class Q013(Solution):
         :type s: str
         :rtype: int
         """
-        numerals = { "M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1 }
-        sum = 0
+        numerals = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+        integer = 0
         s = s[::-1]
         last = None
         for x in s:
             if last and numerals[x] < last:
-                sum -= 2 * numerals[x]
-            sum += numerals[x]
+                integer -= 2 * numerals[x]
+            integer += numerals[x]
             last = numerals[x]
-        return sum
+        return integer
