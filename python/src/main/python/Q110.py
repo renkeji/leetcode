d630a33f1d384774e4807b8194e24c9d9c7089ef
Keji Ren <kren@apple.com>
2015-11-09 23:29:12 -0800

    Balanced Binary Tree

---
+++ python/src/main/python/Q110.py
@@ -0,0 +1,29 @@
+from src.main.python.Solution import Solution
+
+# Given a binary tree, determine if it is height-balanced.
+#
+# For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees
+# of every node never differ by more than 1.
+
+
+class Q110(Solution):
+    class Solution(object):
+        def isBalanced(self, root):
+            """
+            :type root: TreeNode
+            :rtype: bool
+            """
+            def get_height(node):
+                if not node: return 0
+
+                left = get_height(node.left)
+                if left == -1: return -1
+
+                right = get_height(node.right)
+                if right == -1: return -1
+
+                if abs(left - right) > 1: return -1
+
+                return max(left, right) + 1
+
+            return get_height(root) != -1
