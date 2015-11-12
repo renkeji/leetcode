211f1502b2e3b387d4d3a9ec4df50896fc0c152b
Keji Ren <kren@apple.com>
2015-11-12 01:04:27 -0800

    Range Sum Query 2D - Immutable

---
+++ java/src/test/java/solutions/Q304Test.java
@@ -0,0 +1,22 @@
+package solutions;
+
+import junit.framework.TestCase;
+import solutions.Q304.NumMatrix;
+
+public class Q304Test extends TestCase {
+
+    public void testNumMatrix() throws Exception {
+        int[][] matrix = {
+            {3, 0, 1, 4, 2},
+            {5, 6, 3, 2, 1},
+            {1, 2, 0, 1, 5},
+            {4, 1, 0, 1, 7},
+            {1, 0, 3, 0, 5},
+        };
+        NumMatrix numMatrix = new Q304().new NumMatrix(matrix);
+        assertEquals(8, numMatrix.sumRegion(2, 1, 4, 3));
+        assertEquals(11, numMatrix.sumRegion(1, 1, 2, 2));
+        assertEquals(12, numMatrix.sumRegion(1, 2, 2, 4));
+    }
+
+}
