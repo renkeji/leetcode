627d382b36a67d0458e80bd58573f1620d01dffe
Keji Ren <kren@apple.com>
2015-11-25 16:32:10 -0800

    Search Insert Position

---
+++ python/src/main/python/Q035.py
@@ -0,0 +1,33 @@
+from src.main.python.Solution import Solution
+
+# Given a sorted array and a target value, return the index if the target is found. If not, return the index where it
+# would be if it were inserted in order.
+#
+# You may assume no duplicates in the array.
+#
+# Here are few examples.
+# [1,3,5,6], 5 → 2
+# [1,3,5,6], 2 → 1
+# [1,3,5,6], 7 → 4
+# [1,3,5,6], 0 → 0
+
+
+class Q035(Solution):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        if nums:
+            right = len(nums)-1
+            while left <= right:
+                mid = (left+right)//2
+                if nums[mid] == target:
+                    return mid
+                elif nums[mid] < target:
+                    left = mid+1
+                else:
+                    right = mid-1
+        return left
