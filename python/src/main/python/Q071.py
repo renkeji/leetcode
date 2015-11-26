2afd808d41c5f774694153d836a02956df1de0a6
Keji Ren <kren@apple.com>
2015-11-25 17:13:24 -0800

    Simplify Path

--- python/src/main/python/Q071.py
+++ python/src/main/python/Q071.py
@@ -3,15 +3,14 @@ from src.main.python.Solution import Solution
 # Given an absolute path for a file (Unix-style), simplify it.
 #
 # For example,
-# path = "/home/", => "/home"
-# path = "/a/./b/../../c/", => "/c"
-# click to show corner cases.
+#   path = "/home/", => "/home"
+#   path = "/a/./b/../../c/", => "/c"
 #
 # Corner Cases:
-# Did you consider the case where path = "/../"?
-# In this case, you should return "/".
-# Another corner case is the path might contain multiple slashes '/' together, such as "/home//foo/".
-# In this case, you should ignore redundant slashes and return "/home/foo".
+#   Did you consider the case where path = "/../"?
+#   In this case, you should return "/".
+#   Another corner case is the path might contain multiple slashes '/' together, such as "/home//foo/".
+#   In this case, you should ignore redundant slashes and return "/home/foo".
 
 
 class Q071(Solution):
