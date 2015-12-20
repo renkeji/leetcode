a0ba7edadc9be2d04b0677ade1f711dc2e4e74ac
Keji Ren <kren@apple.com>
2015-12-19 21:31:03 -0800

    Reverse Bits

--- java/src/main/java/solutions/Q190.java
+++ java/src/main/java/solutions/Q190.java
@@ -13,11 +13,29 @@ package solutions;
 public class Q190 extends Solution {
 
     public int reverseBits(int n) {
-        n = (n & 0x55555555) << 1 | (n >>> 1) & 0x55555555;
-        n = (n & 0x33333333) << 2 | (n >>> 2) & 0x33333333;
-        n = (n & 0x0f0f0f0f) << 4 | (n >>> 4) & 0x0f0f0f0f;
-        n = (n << 24) | ((n & 0xff00) << 8) | ((n >>> 8) & 0xff00) | (n >>> 24);
+        for (int i = 0; i < 16; i++) {
+            n = swapBits(n, i, 32 - i - 1);
+        }
         return n;
     }
+
+    public int swapBits(int n, int i, int j) {
+        int a = (n >> i) & 1;
+        int b = (n >> j) & 1;
+
+        if ((a ^ b) != 0) {
+            return n ^= (1 << i) | (1 << j);
+        }
+
+        return n;
+    }
+
+//    public int reverseBits(int n) {
+//        n = (n & 0x55555555) << 1 | (n >>> 1) & 0x55555555;
+//        n = (n & 0x33333333) << 2 | (n >>> 2) & 0x33333333;
+//        n = (n & 0x0f0f0f0f) << 4 | (n >>> 4) & 0x0f0f0f0f;
+//        n = (n << 24) | ((n & 0xff00) << 8) | ((n >>> 8) & 0xff00) | (n >>> 24);
+//        return n;
+//    }
 
 }
