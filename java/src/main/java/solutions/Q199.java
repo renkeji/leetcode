90df2eac2f113749ce58deb9ef1b9b1f402ad66e
Keji Ren <kren@apple.com>
2015-12-21 23:07:57 -0800

    Binary Tree Right Side View

--- java/src/main/java/solutions/Q199.java
+++ java/src/main/java/solutions/Q199.java
@@ -20,30 +20,49 @@ import java.util.List;
 
 public class Q199 extends Solution {
 
+    private int maxLevel = 0;
+
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        if (root != null) {
-            List<TreeNode> currLevel = new ArrayList<>();
-            List<TreeNode> nextLevel = new ArrayList<>();
-            currLevel.add(root);
-            while (!currLevel.isEmpty()) {
-                for (int i = 0; i < currLevel.size(); ++i) {
-                    TreeNode curr = currLevel.get(i);
-                    if (curr.left != null) {
-                        nextLevel.add(curr.left);
-                    }
-                    if (curr.right != null) {
-                        nextLevel.add(curr.right);
-                    }
-                    if (i == currLevel.size() - 1) {
-                        ans.add(curr.val);
-                    }
-                }
-                currLevel = nextLevel;
-                nextLevel = new ArrayList<>();
-            }
-        }
+        rightSideView(root, 1, ans);
         return ans;
     }
+
+    private void rightSideView(TreeNode root, int level, List<Integer> ans) {
+        if (root != null) {
+            if (maxLevel < level) {
+                ans.add(root.val);
+                maxLevel = level;
+            }
+            rightSideView(root.right, level+1, ans);
+            rightSideView(root.left, level+1, ans);
+        }
+    }
+
+//    public List<Integer> rightSideView(TreeNode root) {
+//        List<Integer> ans = new ArrayList<>();
+//        if (root != null) {
+//            List<TreeNode> currLevel = new ArrayList<>();
+//            List<TreeNode> nextLevel = new ArrayList<>();
+//            currLevel.add(root);
+//            while (!currLevel.isEmpty()) {
+//                for (int i = 0; i < currLevel.size(); ++i) {
+//                    TreeNode curr = currLevel.get(i);
+//                    if (curr.left != null) {
+//                        nextLevel.add(curr.left);
+//                    }
+//                    if (curr.right != null) {
+//                        nextLevel.add(curr.right);
+//                    }
+//                    if (i == currLevel.size() - 1) {
+//                        ans.add(curr.val);
+//                    }
+//                }
+//                currLevel = nextLevel;
+//                nextLevel = new ArrayList<>();
+//            }
+//        }
+//        return ans;
+//    }
 
 }
