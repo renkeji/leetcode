c9cd6ac6929ec01620b424d7fe43297a241c6fb7
Keji Ren <kren@apple.com>
2015-12-27 22:21:59 -0800

    Coin Change

---
+++ java/src/main/java/solutions/Q322.java
@@ -0,0 +1,43 @@
+package solutions;
+
+import java.util.Arrays;
+
+/**
+ * You are given coins of different denominations and a total amount of money amount. Write a function to compute the
+ * fewest number of coins that you need to make up that amount.
+ * If that amount of money cannot be made up by any combination of the coins, return -1.
+ *
+ * Example 1:
+ *  coins = [1, 2, 5], amount = 11
+ *  return 3 (11 = 5 + 5 + 1)
+ *
+ * Example 2:
+ *  coins = [2], amount = 3
+ *  return -1.
+ *
+ * Note:
+ *  You may assume that you have an infinite number of each kind of coin.
+ */
+
+public class Q322 extends Solution {
+
+    public int coinChange(int[] coins, int amount) {
+        if (coins == null || coins.length == 0) {
+            return -1;
+        } else {
+            Arrays.sort(coins);
+            int[] dp = new int[amount+1];
+            for (int i = 1; i <= amount; ++i) {
+                int min = Integer.MAX_VALUE;
+                for (int j = 0; j < coins.length && i >= coins[j]; ++j) {
+                    if (i >= coins[j] && dp[i-coins[j]] != -1) {
+                        min = Math.min(min, dp[i-coins[j]]);
+                    }
+                }
+                dp[i] = min == Integer.MAX_VALUE ? -1 : min + 1;
+            }
+            return dp[amount];
+        }
+    }
+
+}
