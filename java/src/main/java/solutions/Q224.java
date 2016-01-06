32c9ea93332fd67445c7c28863ee68ef87bf58da
Keji Ren <kren@apple.com>
2016-01-05 23:06:16 -0800

    Basic Calculator

---
+++ java/src/main/java/solutions/Q224.java
@@ -0,0 +1,49 @@
+package solutions;
+
+import java.util.Stack;
+
+/**
+ * Implement a basic calculator to evaluate a simple expression string.
+ *
+ * The expression string may contain open ( and closing parentheses ), the plus + or minus sign -, non-negative integers and empty spaces .
+ *
+ * You may assume that the given expression is always valid.
+ *
+ * Some examples:
+ *  "1 + 1" = 2
+ *  " 2-1 + 2 " = 3
+ *  "(1+(4+5+2)-3)+(6+8)" = 23
+ *
+ * Note: Do not use the eval built-in library function.
+ */
+
+public class Q224 extends Solution {
+
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(1);
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                int num = c - '0';
+                int j = i + 1;
+                while (j < s.length() && Character.isDigit(s.charAt(j))) {
+                    num = 10 * num + (s.charAt(j) - '0');
+                    j++;
+                }
+                res += stack.pop() * num;
+                i = j - 1;
+            } else if (c == '+' || c == '(') {
+                stack.push(stack.peek());
+            } else if (c == '-') {
+                stack.push(-1 * stack.peek());
+            } else if (c == ')') {
+                stack.pop();
+            }
+        }
+        return res;
+    }
+
+}
