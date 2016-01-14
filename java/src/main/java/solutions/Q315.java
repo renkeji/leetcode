c1f0d92a06d90a9f62d54a56bb001f1678d433bb
Keji Ren <kren@apple.com>
2016-01-14 01:39:26 -0800

    Count of Smaller Numbers After Self

--- java/src/main/java/solutions/Q315.java
+++ java/src/main/java/solutions/Q315.java
@@ -23,49 +23,104 @@ public class Q315 extends Solution {
     public List<Integer> countSmaller(int[] nums) {
         List<Integer> ans = new ArrayList<>();
         if (nums != null && nums.length > 0) {
-            Node root = new Node(nums[nums.length - 1]);
-            ans.add(0);
-            for (int i = nums.length - 2; i >= 0; --i) {
-                ans.add(insert(root, nums[i]));
+            int n = nums.length;
+            for (int i = 0; i < n; ++i) {
+                ans.add(0);
             }
-            Collections.reverse(ans);
+            Pair[] pairs = new Pair[n];
+            for (int i = 0; i < n; ++i) {
+                pairs[i] = new Pair(nums[i], i);
+            }
+            sort(pairs, 0, n-1, ans);
         }
         return ans;
     }
 
-    private int insert(Node root, int num) {
-        int count = 0;
-        while (true) {
-            if (root.val >= num) {
-                root.count++;
-                if (root.left == null) {
-                    root.left = new Node(num);
-                    break;
-                } else {
-                    root = root.left;
-                }
-            } else {
-                count += root.count;
-                if (root.right == null) {
-                    root.right = new Node(num);
-                    break;
-                } else {
-                    root = root.right;
-                }
-            }
+    private void sort(Pair[] pairs, int low, int high, List<Integer> ans) {
+        if (low < high) {
+            int mid = low + (high - low) / 2;
+            sort(pairs, low, mid, ans);
+            sort(pairs, mid+1, high, ans);
+            merge(pairs, low, mid, high, ans);
         }
-        return count;
     }
 
-    private class Node {
+    private void merge(Pair[] pairs, int low, int mid, int high, List<Integer> ans) {
+        int n = high - low + 1;
+        int i = mid, k = n - 1, j = high;
+        Pair[] temp = new Pair[n];
+        System.arraycopy(pairs, low, temp, 0, n);
+        while (i >= low && j >= mid+1) {
+            if (pairs[i].val > pairs[j].val) {
+                int index = pairs[i].index;
+                int value = ans.get(index) + j - mid;
+                ans.set(index, value);
+                temp[k--] = pairs[i--];
+            } else {
+                temp[k--] = pairs[j--];
+            }
+        }
+        while (j >= mid+1) {
+            temp[k--] = pairs[j--];
+        }
+        System.arraycopy(temp, 0, pairs, low, n);
+    }
+
+    private class Pair {
         int val;
-        Node left;
-        Node right;
-        int count;
-        Node(int value) {
-            this.val = value;
-            this.count = 1;
+        int index;
+        Pair(int val, int index) {
+            this.val = val;
+            this.index = index;
         }
     }
+
+//    public List<Integer> countSmaller(int[] nums) {
+//        List<Integer> ans = new ArrayList<>();
+//        if (nums != null && nums.length > 0) {
+//            Node root = new Node(nums[nums.length - 1]);
+//            ans.add(0);
+//            for (int i = nums.length - 2; i >= 0; --i) {
+//                ans.add(insert(root, nums[i]));
+//            }
+//            Collections.reverse(ans);
+//        }
+//        return ans;
+//    }
+//
+//    private int insert(Node root, int num) {
+//        int count = 0;
+//        while (true) {
+//            if (root.val >= num) {
+//                root.count++;
+//                if (root.left == null) {
+//                    root.left = new Node(num);
+//                    break;
+//                } else {
+//                    root = root.left;
+//                }
+//            } else {
+//                count += root.count;
+//                if (root.right == null) {
+//                    root.right = new Node(num);
+//                    break;
+//                } else {
+//                    root = root.right;
+//                }
+//            }
+//        }
+//        return count;
+//    }
+//
+//    private class Node {
+//        int val;
+//        Node left;
+//        Node right;
+//        int count;
+//        Node(int value) {
+//            this.val = value;
+//            this.count = 1;
+//        }
+//    }
 
 }
