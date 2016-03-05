2b595249c636895201644e5993d8f3f19c5c52ba
Keji Ren <kren@apple.com>
2016-03-04 22:09:46 -0800

    Longest Substring with At Most Two Distinct Characters

--- java/src/main/java/solutions/Q159.java
+++ java/src/main/java/solutions/Q159.java
@@ -13,30 +13,72 @@ import java.util.Map;
 
 public class Q159 extends Solution {
 
+//    public int lengthOfLongestSubstringTwoDistinct(String s) {
+//        if (s == null || s.isEmpty()) {
+//            return 0;
+//        }
+//        Map<Character, Integer> hm = new HashMap<>();
+//        int i = 0, j = 0;
+//        int max = Integer.MIN_VALUE;
+//        while (j < s.length()) {
+//            if (!hm.containsKey(s.charAt(j))) {
+//                if (hm.size() == 2) {
+//                    max = Math.max(max, j - i);
+//                    while (i < j && hm.get(s.charAt(i)) != 1) {
+//                        hm.put(s.charAt(i), hm.get(s.charAt(i)) - 1);
+//                        ++i;
+//                    }
+//                    hm.remove(s.charAt(i));
+//                    ++i;
+//                }
+//                hm.put(s.charAt(j), 0);
+//            }
+//            hm.put(s.charAt(j), hm.get(s.charAt(j)) + 1);
+//            ++j;
+//        }
+//        return Math.max(max, j - i);
+//    }
+
     public int lengthOfLongestSubstringTwoDistinct(String s) {
-        if (s == null || s.isEmpty()) {
-            return 0;
-        }
-        Map<Character, Integer> hm = new HashMap<>();
-        int i = 0, j = 0;
-        int max = Integer.MIN_VALUE;
-        while (j < s.length()) {
-            if (!hm.containsKey(s.charAt(j))) {
-                if (hm.size() == 2) {
+        int max = 0;
+        if (s != null && !s.isEmpty()) {
+            char first = ' ', second = ' ';
+            int firstCnt = 0, secondCnt = 0;
+            int i = 0, j = 0;
+            int len = s.length();
+            char[] charArray = s.toCharArray();
+            while (j < len) {
+                char ch = charArray[j];
+                if (first == ' ' || ch == first) {
+                    first = ch;
+                    firstCnt++;
+                } else if (second == ' ' || ch == second) {
+                    second = ch;
+                    secondCnt++;
+                } else {
                     max = Math.max(max, j - i);
-                    while (i < j && hm.get(s.charAt(i)) != 1) {
-                        hm.put(s.charAt(i), hm.get(s.charAt(i)) - 1);
-                        ++i;
+                    while (i < j) {
+                        char c = charArray[i++];
+                        if (c == first) {
+                            if (--firstCnt == 0) {
+                                first = ch;
+                                firstCnt = 1;
+                                break;
+                            }
+                        } else {
+                            if (--secondCnt == 0) {
+                                second = ch;
+                                secondCnt = 1;
+                                break;
+                            }
+                        }
                     }
-                    hm.remove(s.charAt(i));
-                    ++i;
                 }
-                hm.put(s.charAt(j), 0);
+                ++j;
             }
-            hm.put(s.charAt(j), hm.get(s.charAt(j)) + 1);
-            ++j;
+            max = Math.max(max, j - i);
         }
-        return Math.max(max, j - i);
+        return max;
     }
 
 }
