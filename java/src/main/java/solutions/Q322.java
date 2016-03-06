16d104cfa5b55ea8a4cffab57df7eec35efd9012
Keji Ren <kren@apple.com>
2016-03-06 14:28:56 -0800

    Coin Change

--- java/src/main/java/solutions/Q322.java
+++ java/src/main/java/solutions/Q322.java
@@ -1,7 +1,5 @@
 package solutions;
 
-import java.util.Arrays;
-
 /**
  * You are given coins of different denominations and a total amount of money amount. Write a function to compute the
  * fewest number of coins that you need to make up that amount.
