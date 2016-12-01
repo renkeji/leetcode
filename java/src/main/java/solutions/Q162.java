8f068ee97fd3222629bd6007ed9b7e0876dc4358
Keji Ren <kren@apple.com>
2016-12-01 15:18:02 -0800

    Find Peak Element

--- java/src/main/java/solutions/Q162.java
+++ java/src/main/java/solutions/Q162.java
@@ -18,13 +18,13 @@ public class Q162 extends Solution {
         if (nums == null || nums.length == 0) {
             return -1;
         }
-        int left = 0, right = nums.length-1;
+        int left = 0, right = nums.length - 1;
         while (left <= right) {
-            int mid = left+(right-left)/2;
-            if (mid+1 < nums.length && nums[mid] < nums[mid+1]) {
-                left = mid+1;
-            } else if (mid-1 >= 0 && nums[mid] < nums[mid-1]) {
-                right = mid-1;
+            int mid = left + (right - left) / 2;
+            if (mid + 1 < nums.length && nums[mid] < nums[mid+1]) {
+                left = mid + 1;
+            } else if (mid - 1 >= 0 && nums[mid] < nums[mid-1]) {
+                right = mid - 1;
             } else {
                 return mid;
             }
