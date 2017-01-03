1446a0694d101785788063a701876b9d18b33f3b
Keji Ren <kren@apple.com>
2017-01-03 12:32:10 -0800

    Move Zeroes

--- java/src/main/java/solutions/Q283.java
+++ java/src/main/java/solutions/Q283.java
@@ -14,37 +14,29 @@ public class Q283 extends Solution {
 
     // This algorithm moves the zeros to back
     public void moveZeroes(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return;
+        if (nums == null || nums.length == 0) return;
+
+        int i = 0;
+        for (int num: nums) {
+            if (num != 0) nums[i++] = num;
         }
-        for (int i = 0, j = 0; j < nums.length; ++j) {
-            if (nums[j] != 0) {
-                while (i < j && nums[i] != 0) {
-                    ++i;
-                }
-                if (i != j) {
-                    nums[i] = nums[j];
-                    nums[j] = 0;
-                }
-            }
+
+        while (i < nums.length) {
+            nums[i++] = 0;
         }
     }
 
     // This algorithm moves the zeros to front
     public void moveZerosToFront(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return;
+        if (nums == null || nums.length == 0) return;
+
+        int i = nums.length - 1;
+        for (int j = nums.length - 1; j >= 0; --j) {
+            if (nums[j] != 0) nums[i--] = nums[j];
         }
-        for (int i = nums.length - 1, j = nums.length - 1; j >= 0; --j) {
-            if (nums[j] != 0) {
-                while (i > j && nums[i] != 0) {
-                    --i;
-                }
-                if (i != j) {
-                    nums[i] = nums[j];
-                    nums[j] = 0;
-                }
-            }
+
+        while (i >= 0) {
+            nums[i--] = 0;
         }
     }
 
