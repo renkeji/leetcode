f2167bcf440515cc0f77d4e7a479bea1bc3163f7
Keji Ren <kren@apple.com>
2017-01-04 00:25:21 -0800

    Integer to English Words

--- java/src/main/java/solutions/Q273.java
+++ java/src/main/java/solutions/Q273.java
@@ -11,47 +11,36 @@ package solutions;
 
 public class Q273 extends Solution {
 
-    public String convertThree(int n) {
-        String[] ONES = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"};
-        String[] ONE_TENS = {"Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-        String[] TENS = {"Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-        String ans = "";
-        int hundred = n / 100;
-        if (hundred != 0) {
-            ans += ONES[hundred-1] + " Hundred";
-        }
-
-        int ten = n % 100;
-        if (ten >= 11 && ten <= 19) {
-            ans += " " + ONE_TENS[ten-11];
-        } else if (ten > 19) {
-            ans += " " + TENS[ten/10-2];
-            if (ten % 10 != 0) {
-                ans += " " + ONES[ten%10-1];
-            }
-        } else if (ten > 0 && ten <= 10) {
-            ans += " " + ONES[ten-1];
-        }
-
-        return ans.trim();
-    }
+    private final String[] LESS_THAN_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+        "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    private final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
 
     public String numberToWords(int num) {
-        if (num == 0) {
-            return "Zero";
-        }
-        String[] UNITS = {"", "Thousand", "Million", "Billion"};
-        String ans = "";
-        int i = 9;
+        if (num == 0) return "Zero";
+
+        int i = 0;
+        String words = "";
+
         while (num > 0) {
-            if (num >= Math.pow(10, i)) {
-                int n = num / (int)Math.pow(10, i);
-                ans += " " + convertThree(n) + " " + UNITS[i/3];
-                num -= n * (int)Math.pow(10, i);
-            }
-            i -= 3;
+            if (num % 1000 != 0)
+                words = helper(num % 1000) +THOUSANDS[i] + " " + words;
+            num /= 1000;
+            i++;
         }
-        return ans.trim();
+
+        return words.trim();
+    }
+
+    private String helper(int num) {
+        if (num == 0)
+            return "";
+        else if (num < 20)
+            return LESS_THAN_20[num] + " ";
+        else if (num < 100)
+            return TENS[num / 10] + " " + helper(num % 10);
+        else
+            return LESS_THAN_20[num / 100] + " Hundred " + helper(num % 100);
     }
 
 }
