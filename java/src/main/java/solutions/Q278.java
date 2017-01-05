fdb86b77bd9ebe7fe649bbf728bc867e4bf62032
Keji Ren <kren@apple.com>
2017-01-04 16:43:36 -0800

    First Bad Version

--- java/src/main/java/solutions/Q278.java
+++ java/src/main/java/solutions/Q278.java
@@ -19,21 +19,11 @@ public class Q278 extends Solution {
     }
 
     public int firstBadVersion(int n) {
-        if (n <= 0) {
-            return 0;
-        }
         int left = 1, right = n;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (isBadVersion(mid)) {
-                if (mid == 1 || !isBadVersion(mid-1)) {
-                    return mid;
-                } else {
-                    right = mid - 1;
-                }
-            } else {
-                left = mid + 1;
-            }
+        while (left < right) {
+            int mid = left + (right - left >> 1);
+            if (!isBadVersion(mid)) left = mid + 1;
+            else right = mid;
         }
         return left;
     }
